8b6de88a71e289e6d3b615e67224993181fbac00
Pedro-Moran <79230714+Pedro-Moran@users.noreply.github.com>
2025-11-24 15:53:43 -0500

    Corrige paginacion en pruebas de FMC7Connection

--- onl_orquestadora_ffmm/artifact/dtos/PFMHC010/src/main/java/com/bbva/pfmh/dto/jcisconnector/ffmm/commons/LinksDTO.java
+++ onl_orquestadora_ffmm/artifact/dtos/PFMHC010/src/main/java/com/bbva/pfmh/dto/jcisconnector/ffmm/commons/LinksDTO.java
@@ -44,6 +44,27 @@ public class LinksDTO implements Serializable {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LinksDTO linksDTO = (LinksDTO) o;
+        return java.util.Objects.equals(first, linksDTO.first)
+                && java.util.Objects.equals(last, linksDTO.last)
+                && java.util.Objects.equals(previous, linksDTO.previous)
+                && java.util.Objects.equals(next, linksDTO.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(first, last, previous, next);
+    }
+
+    @Override
     public String toString() {
         return "DTOLinks{" +
                 "first='" + first + '\'' +
